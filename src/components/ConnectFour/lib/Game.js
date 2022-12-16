8c5a50860b2e6588dcfbfab044c73c0a94b3f0ba
chriszack3 <czack@chrispc.localdomain>
2022-12-16 15:51:17 -0500

    drop tile logic

--- src/components/ConnectFour/lib/Game.js
+++ src/components/ConnectFour/lib/Game.js
@@ -5,13 +5,35 @@ const Game = ({ context }) => {
     const { gameState, setGameState } = useContext(context);
     const { players, turn, selected } = gameState;
     
+    const handleSelect = (e) => {
+        e.preventDefault();
+        const newBoard = [...gameState.board];
+        for (let i = newBoard.length - 1; i >= 0; i--) {
+            if (newBoard[i][selected[1]].color === null) {
+                newBoard[i][selected[1]].color = turn % 2 === 0 ? players[0].color : players[1].color;
+                break;
+            }
+        }
+        setGameState({
+            ...gameState,
+            board: newBoard,
+            turn: turn + 1,
+            selected: []
+        })
+    }
+
     return (
         <div>
             {
                 turn % 2 === 0 ? <h1>{players[0].name}'s Turn</h1> : <h1>{players[1].name}'s Turn. <br/> Please Select a Column to Drop your Tile</h1>
             }
             {
-                selected.length > 0 && <h1>Selected: {selected[0]}, {selected[1]}</h1>
+                selected.length > 0 && (
+                    <form onSubmit={(e) => handleSelect(e, selected[1])} >
+                        <h1>Drop Tile into Column {selected[1] + 1}</h1>
+                        <button type="submit">Confirm?</button>
+                    </form>
+                )
             }
             <Table context={context} />
         </div>
